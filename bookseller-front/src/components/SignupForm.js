444349f58368feecb3d57006dfc2b15228ca9937
Antony Lo <mrmikadox@gmail.com>
2022-05-10 14:22:32 +0800

    set up signup form

---
+++ bookseller-front/src/components/SignupForm.js
@@ -0,0 +1,17 @@
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const SignupForm = () => {
+  return (
+    <>
+      <Form>
+        <Form.Group></Form.Group>
+      </Form>
+    </>
+  );
+}
+
+export default SignupForm;
