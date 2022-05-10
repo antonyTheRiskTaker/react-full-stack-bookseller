f0159890f70a7e07f813979ad9e5ada92d34e168
Antony Lo <mrmikadox@gmail.com>
2022-05-10 19:25:25 +0800

    add some book topics as navdropdown items

--- bookseller-front/src/components/NavbarDropdown.js
+++ bookseller-front/src/components/NavbarDropdown.js
@@ -4,11 +4,21 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const NavbarDropdown = () => {
   return (
     <>
-    <Nav>
-      <NavDropdown>
-        <NavDropdown.Item></NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
+        <NavDropdown
+          id='navbar-dropdown-menu'
+          title='Books'
+          menuVariant='#FFFFF0'
+        >
+          <NavDropdown.Item href='books/finance'>
+            Finance
+          </NavDropdown.Item>
+          <NavDropdown.Item href='books/history'>
+            History
+          </NavDropdown.Item>
+          <NavDropdown.Item href='books/philosophy'>
+            Philosophy
+          </NavDropdown.Item>
+        </NavDropdown>
     </>
   );
 }
