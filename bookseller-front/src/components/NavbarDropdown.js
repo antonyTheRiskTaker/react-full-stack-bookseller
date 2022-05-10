24954201d546533e66251fabe2b1ff89c1bba84b
Antony Lo <mrmikadox@gmail.com>
2022-05-10 16:36:36 +0800

    add Nav, NavDropdown & NavDropdown.Item

--- bookseller-front/src/components/NavbarDropdown.js
+++ bookseller-front/src/components/NavbarDropdown.js
@@ -3,7 +3,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const NavbarDropdown = () => {
   return (
-    <></>
+    <>
+    <Nav>
+      <NavDropdown>
+        <NavDropdown.Item></NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    </>
   );
 }
 
