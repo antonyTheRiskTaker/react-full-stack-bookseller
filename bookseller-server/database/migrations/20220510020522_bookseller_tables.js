f8a46adc39913592700c08c1e3b4ca9b33297068
Antony Lo <mrmikadox@gmail.com>
2022-05-10 12:17:09 +0800

    set up database migrations

--- bookseller-server/database/migrations/20220510020522_bookseller_tables.js
+++ bookseller-server/database/migrations/20220510020522_bookseller_tables.js
@@ -16,16 +16,29 @@ exports.up = function(knex) {
     table.string('email').notNullable().unique();
     table.string('password').notNullable();
     table.string('address');
-    table.boolean('admin_access'); // need review
+    table.boolean('admin_access');
   }).then(() => {
     return knex.schema.createTable('credit_card', table => {
       table.increments('id').primary();
       table.integer('user_id').unsigned();
       table.foreign('user_id').references('user.id');
       table.string('cardholder_name').notNullable();
-      table.string('expiration_date').notNullable(); // need review
-      table.string('cvv').notNullable(); // need review
-      table.boolean('')
+      table.string('expiration_date').notNullable();
+    })
+  }).then(() => {
+    return knex.schema.createTable('author', table => {
+      table.increments('id').primary();
+      table.string('first_name').notNullable();
+      table.string('middle_name');
+      table.string('last_name').notNullable();
+    })
+  })
+  .then(() => {
+    return knex.schema.createTable('book', table => {
+      table.integer('ibsn').primary();
+      table.string('title').notNullable();
+      table.string('synopsis');
+      table.string
     })
   })
 };
