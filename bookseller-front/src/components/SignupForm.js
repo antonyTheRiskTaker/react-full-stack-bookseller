684816f4a71c81538f0c58b1d1df6412c0753721
Antony Lo <mrmikadox@gmail.com>
2022-05-10 14:49:18 +0800

    add fname lname email username & password

--- bookseller-front/src/components/SignupForm.js
+++ bookseller-front/src/components/SignupForm.js
@@ -8,7 +8,45 @@ const SignupForm = () => {
   return (
     <>
       <Form>
-        <Form.Group></Form.Group>
+        <Container>
+          <Row>
+            <Col>
+              <Form.Group className='mb-3' controlId='signupFirstName'>
+                <Form.Label>First name*</Form.Label>
+                <Form.Control type='text' />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className='mb-3' controlId='signupLastName'>
+                <Form.Label>Last name*</Form.Label>
+                <Form.Control type='text' />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className='mb-3' controlId='signupEmail'>
+                <Form.Label>Email*</Form.Label>
+                <Form.Control type='email' />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group className='mb-3' controlId='signupUsername'>
+                <Form.Label>Username*</Form.Label>
+                <Form.Control type='text' />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group className='mb-3' controlId='signupPassword'>
+                <Form.Label>Password*</Form.Label>
+                <Form.Control type='password' />
+                <Form.Text></Form.Text>
+              </Form.Group>
+            </Col>
+          </Row>
+        </Container>
       </Form>
     </>
   );
