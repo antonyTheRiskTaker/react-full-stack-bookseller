cd62fdd40e1205ac4530f63e8c08bd0a10541077
Antony Lo <mrmikadox@gmail.com>
2022-05-11 17:45:28 +0800

    add implementations of routers and services and finalise app.js

---
+++ bookseller-server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const e = require('express');
+
+const knexfile = require('./knexfile').development;
+const knex = require('knex')(knexfile);
+
+const auth = require('./auth/auth')(knex);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+// (Line below) it allows us to take the values that we'd get from a form input
+app.use(express.urlencoded({ extended: false }));
+app.use(auth.initialize());
+
+// Set up routers and services for webapp (to be deleted later)
+// e.g. 
+// const TodoRouter = require('./Routers/TodoRouter');
+// const TodoService = require('./Services/TodoService');
+// const AuthRouter = require('./Routers/AuthRouter');
+
+// Set up routers and services for bookseller
+const ManageBookRouter = require('./Routers/ManageBookRouter');
+const ManageBookService = require('./Services/ManageBookService');
+
+// const todoService = new TodoService(knex);
+
+// app.use('/api', new TodoRouter(todoService, authClass).router());
+// app.use('/auth', new AuthRouter(express).router());
+
+app.listen(8080, () => {
+  console.log('Application listening to port 8080');
+});
