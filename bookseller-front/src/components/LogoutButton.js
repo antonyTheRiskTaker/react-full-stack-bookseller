bc7a767a56c3e8d872fbfc818cf806ffa3be0256
Antony Lo <mrmikadox@gmail.com>
2022-05-14 16:53:41 +0800

    set up logout button component

---
+++ bookseller-front/src/components/LogoutButton.js
@@ -0,0 +1,17 @@
+import { logoutNowThunk } from '../redux/auth/actions';
+
+import { useDispatch } from 'react-redux';
+
+import {Button} from 'react-bootstrap';
+
+export default function LogoutButton() {
+  let dispatch = useDispatch();
+
+  return (
+    <div>
+      <Button variant='info' onClick={() => dispatch(logoutNowThunk)}>
+        Logout
+      </Button>
+    </div>
+  );
+}
