10f6503a26539bc52fccf356839f862105b63bce
Antony Lo <mrmikadox@gmail.com>
2022-05-14 17:49:52 +0800

    minor modification to App.js

--- bookseller-front/src/App.js
+++ bookseller-front/src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HomeNavbar />
+        <HomeNavbar authentication={isAuthenticated}/>
       </header>
       {/* <Container>
         <FormDemo />
