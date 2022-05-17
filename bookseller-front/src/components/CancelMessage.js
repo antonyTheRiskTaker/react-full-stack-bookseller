b29603543c307fb64b87a4fc6133bf6b2823a5cf
Antony Lo <mrmikadox@gmail.com>
2022-05-17 14:45:26 +0800

    success & cancel checkout pages redirect to homepage after 5 secs

--- bookseller-front/src/components/CancelMessage.js
+++ bookseller-front/src/components/CancelMessage.js
@@ -1,7 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const CancelMessage = () => {
   const message = "Order canceled -- continue to shop around and checkout when you're ready.";
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 5000);
+  }, [navigate]);
+  
+  // const eventHandler = () => {
+  //   setTimeout(() => {
+  //     navigate('/');
+  //   }, 5000);
+  //   navigate('/');
+  // };
+
   return (
+    <section>
       <p>{message}</p>
+      <p>You will be redirected back to the homepage in 5 seconds...</p>
+    </section>
   );
 }
 
--- bookseller-front/src/components/SuccessMessage.js
+++ bookseller-front/src/components/SuccessMessage.js
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 const SuccessMessage = () => {
   const message = 'Order placed! You will receive an email confirmation.';
+  const navigate = useNavigate();
+  // const onLoadHandler = () => {
+  //   setTimeout(() => {
+  //     navigate('/');
+  //   }, 5000);
+  // };
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 5000);
+  }, [navigate]);
+  
   return (
+    <section>
       <p>{message}</p>
+      <p>You will be redirected back to the homepage in 5 seconds...</p>
+    </section>
   );
 }
 
