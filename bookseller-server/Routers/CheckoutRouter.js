04c4105b4a2d7d52484c52cdcf68acc337a31749
Antony Lo <mrmikadox@gmail.com>
2022-05-17 09:54:47 +0800

    rewrite line_items in stripe post request route

--- bookseller-server/Routers/CheckoutRouter.js
+++ bookseller-server/Routers/CheckoutRouter.js
@@ -21,8 +21,9 @@ class CheckoutRouter {
   }
   async processPayment(req, res) {
     console.log(req.body.cartItems)
+    const cartItems = req.body.cartItems;
     let initialValue = 0;
-    const total = req.body.cartItems.reduce((previousValue, currentValue) => {
+    const total = cartItems.reduce((previousValue, currentValue) => {
       return previousValue + currentValue.price_per_unit;
     }, initialValue);
     const totalInCents = 100 * total;
@@ -31,21 +32,12 @@ class CheckoutRouter {
       const session = await this.stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         mode: 'payment',
-        line_items: req.body.cartItems.map(item => {
-          // const cartItem = cart.get(item.isbn);
-          // Get the total price
-          // iterate through cartItems - get the sum using reduce. 
-          return {
-            price_data: {
-              currency: 'hkd',
-              product_data: {
-                name: storeItem.title
-              },
-              unit_amount: storeItem.priceInCents
-            },
-            quantity: item.quantity
-          }
-        }),
+        line_items: [
+          {
+            price: totalInCents,
+            quantity: cartItems.length
+          },
+        ],
         success_url: `${process.env.CLIENT_URL}?success=true`,
         cancel_url: `${process.env.CLIENT_URL}/?cancel=true`
       })
