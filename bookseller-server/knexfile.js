123347fa5c25197ab0f915701d229631dcfd673f
antonyTheRiskTaker <mrmikadox@gmail.com>
2022-09-27 08:39:52 +0800

    configured knex file for web hosting

--- bookseller-server/knexfile.js
+++ bookseller-server/knexfile.js
@@ -12,7 +12,9 @@ module.exports = {
     connection: {
       database: process.env.DB_NAME,
       user: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
     },
     pool: {
       min: 2,
